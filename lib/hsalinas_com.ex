a4eda729b6cdfe6fc177dc5d288781971fa7b1ad
Héctor Salinas <hector_sag@outlook.com>
2023-12-19 10:08:08 -0600

    Initial commit

---
+++ lib/hsalinas_com.ex
@@ -0,0 +1,18 @@
+defmodule HsalinasCom do
+  @moduledoc """
+  Documentation for `HsalinasCom`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HsalinasCom.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
